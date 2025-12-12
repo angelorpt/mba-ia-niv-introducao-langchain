22bef560b8cfa8e5914d20af0d06e9d77a6d3e48
Angelo Rafael <angelorpt@gmail.com>
2025-12-12 10:48:05 -0400

    Revert "feat: Testes pessoais de LangChain"
    This reverts commit d07a6f35b76011bf73b144d4d4ba0fa75109d8ae.

--- 1-fundamentos/2-init-chat-model.py
+++ 1-fundamentos/2-init-chat-model.py
@@ -2,7 +2,6 @@ from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 load_dotenv()
 
-model = init_chat_model(model="gemini-2.5-flash", model_provider="google_genai")
-message = model.invoke("Hello World")
-
-print(message)
+gemini = init_chat_model(model="gemini-2.5-flash", model_provider="google_genai")
+answer_gemini = gemini.invoke("Hello World")
+print(answer_gemini.content)
--- 1-fundamentos/3-prompt-template.py
+++ 1-fundamentos/3-prompt-template.py
@@ -2,8 +2,8 @@ from langchain.prompts import PromptTemplate
 
 template = PromptTemplate(
     input_variables=["name"],
-    template="Oi, Me chamo {name}! Fala uma coisa interessante sobre meu nome."
+    template="Hi, I'm {name}! Tell me a joke with my name!"
 )
 
-text = template.format(name="Angelo")
+text = template.format(name="Wesley")
 print(text)
